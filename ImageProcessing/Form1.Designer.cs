8655b3093c67ae9ec3e184b8cf77e978ba438f6e
Caden Fletcher <Caden.M.Fletcher@gmail.com>
2023-03-16 18:22:47 -0500

    Add project files.

---
+++ ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PictureUnaltered = new PictureBox();
+            PictureAltered = new PictureBox();
+            ButtonBrowse = new Button();
+            ButtonConvertToGrayscale = new Button();
+            ((System.ComponentModel.ISupportInitialize)PictureUnaltered).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PictureAltered).BeginInit();
+            SuspendLayout();
+            // 
+            // PictureUnaltered
+            // 
+            PictureUnaltered.Location = new Point(12, 12);
+            PictureUnaltered.Name = "PictureUnaltered";
+            PictureUnaltered.Size = new Size(300, 300);
+            PictureUnaltered.TabIndex = 0;
+            PictureUnaltered.TabStop = false;
+            // 
+            // PictureAltered
+            // 
+            PictureAltered.Location = new Point(488, 12);
+            PictureAltered.Name = "PictureAltered";
+            PictureAltered.Size = new Size(300, 300);
+            PictureAltered.TabIndex = 1;
+            PictureAltered.TabStop = false;
+            // 
+            // ButtonBrowse
+            // 
+            ButtonBrowse.Location = new Point(12, 318);
+            ButtonBrowse.Name = "ButtonBrowse";
+            ButtonBrowse.Size = new Size(75, 23);
+            ButtonBrowse.TabIndex = 2;
+            ButtonBrowse.Text = "Browse";
+            ButtonBrowse.UseVisualStyleBackColor = true;
+            ButtonBrowse.Click += ButtonBrowse_Click;
+            // 
+            // ButtonConvertToGrayscale
+            // 
+            ButtonConvertToGrayscale.Location = new Point(488, 318);
+            ButtonConvertToGrayscale.Name = "ButtonConvertToGrayscale";
+            ButtonConvertToGrayscale.Size = new Size(139, 23);
+            ButtonConvertToGrayscale.TabIndex = 3;
+            ButtonConvertToGrayscale.Text = "Convert To Grayscale";
+            ButtonConvertToGrayscale.UseVisualStyleBackColor = true;
+            ButtonConvertToGrayscale.Click += ButtonConvertToGrayscale_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ButtonConvertToGrayscale);
+            Controls.Add(ButtonBrowse);
+            Controls.Add(PictureAltered);
+            Controls.Add(PictureUnaltered);
+            Name = "Form1";
+            Text = "Image Processor";
+            ((System.ComponentModel.ISupportInitialize)PictureUnaltered).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PictureAltered).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox PictureUnaltered;
+        private PictureBox PictureAltered;
+        private Button ButtonBrowse;
+        private Button ButtonConvertToGrayscale;
+    }
+}
---
+++ ImageProcessing/Form1.cs
@@ -0,0 +1,52 @@
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBrowse_Click(object sender, EventArgs e) // Import Image
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                PictureUnaltered.Image = new Bitmap(openFile.FileName);
+                PictureAltered.Image = new Bitmap(openFile.FileName);
+            }
+        }
+
+        private void ButtonConvertToGrayscale_Click(object sender, EventArgs e) // Convert Image to Grayscale
+        {
+            Bitmap copyBitmap = new Bitmap((Bitmap)PictureAltered.Image);
+            ProcessImage(copyBitmap);
+            PictureAltered.Image = copyBitmap;
+        }
+
+        private void ProcessImage(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int gray = (byte)(.299 * red + .587 * green + .114 * blue);
+
+                    red = gray;
+                    green = gray;
+                    blue = gray;
+
+                    bmp.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                }
+            }
+        }
+    }
+}
